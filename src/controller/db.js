ba038b6f3534bf83c3d43f54a862d48f2960a568
saikrishna-muppa <39872073+saikrishna-muppa@users.noreply.github.com>
2021-05-20 19:57:52 +0530

    added index file

--- src/controller/db.js
+++ src/controller/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-// const MONGODBURI="mongodb+srv://root:<password>@cluster0.yx0gr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const MONGODBURI = "mongodb://localhost/skilllibetryapi";
+const MONGODBURI =
+  "mongodb+srv://skill:skill@onlineshopping.jm8uf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const MONGODBURI = "mongodb://localhost/skilllibetryapi";
 
 const InitiateMongoServer = async () => {
   try {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 
 const app = express();
-app.listen(process.env.PORT || 3000, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("server running on port 3000");
-  }
+const bodyparser = require("body-parser");
+const InitiateMongoServer = require("./controller/db");
+const user = require("./routes/AuthRoute");
+const cros = require("cors");
+
+InitiateMongoServer();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyparser.json());
+app.use(cors({ exposedHeaders: ["Authorization"] }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/user", user);
+
+app.get("/", function (req, res) {
+  res.json(`welcome to login,signup api`);
+});
+
+app.listen(PORT, () => {
+  console.log(` app is live at ${PORT}`);
 });
